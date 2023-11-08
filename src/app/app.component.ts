e64246e1d4a8c632f8b67682dab5a3d9a2773184
Liviu <liviu@luxon-tech.com>
2023-11-08 18:51:10 +0200

    lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './interfaces/student';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fmi2023';
+  showAlert: boolean | null | number = true;
+
+  fructe: string[] = ['mar', 'para', 'banana'];
+  studenti: Student[] = [
+    {
+      nume: 'Ionel',
+      nota: 5,
+      matematica: 12
+    },
+    {
+      nume: 'Gigel',
+      nota: 7
+    },
+    {
+      nume: 'Ana',
+      nota: 9
+    }
+  ];
+
+  doHideOrShowAlert() {
+    this.showAlert = !this.showAlert;
+  }
+
+  adaugaFruct(fruct: string) {
+    this.fructe.push(fruct);
+  }
+
+  exmatriculeaza(student: Student) {
+    console.log('EXMATRICULEAZA', student);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsRowComponent } from './components/students-row/students-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsRowComponent
   ],
   imports: [
     BrowserModule,
